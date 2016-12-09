47eb91498f074193af79119fc9a91b845cd417c5
SlashMagic <ryanm.lee12@hotmail.com>
2016-12-09 09:32:04 -0500

    just the start

---
+++ src/stupid/packages/Main.java
@@ -0,0 +1,114 @@
+package stupid.packages;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.newdawn.slick.Color;
+
+public class Main {
+
+	int width = 960;
+	int height = 540;
+	
+	boolean isFullScreen = true;
+	
+	boolean exit = false;
+	
+	long lastFPS;
+	long lastFrame;
+	
+	int fps;
+	
+	public void start(){
+		
+		try{
+			if(isFullScreen){
+				Display.setDisplayModeAndFullscreen(Display.getDesktopDisplayMode());
+			}
+			else if(!isFullScreen){
+				Display.setDisplayMode(new DisplayMode(width, height));
+			}
+			
+			Display.create();
+			Keyboard.create();
+			
+		} catch (LWJGLException e){
+			e.printStackTrace();
+			System.out.print("WTF");
+			
+		}
+
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+	    GL11.glShadeModel(GL11.GL_SMOOTH);        
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glDisable(GL11.GL_LIGHTING);                    
+	 
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);                
+	    GL11.glClearDepth(1);                                       
+	 
+	    GL11.glEnable(GL11.GL_BLEND);
+	    GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+			 
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, height, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
+		World gameWorld = new World();
+		
+		
+		while(!Display.isCloseRequested() && !exit){
+			
+			updateFPS();
+			
+			Keyboard.poll();
+			
+			if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
+				exit = true;
+			}
+			
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			
+			Color.white.bind();
+			
+			gameWorld.update(getDelta());
+			
+			Display.update();
+			
+			Display.sync(144);
+			
+		}
+		
+		Display.destroy();
+		
+	}
+	
+	public void updateFPS(){
+		if (getTime() - lastFPS > 1000) {
+			Display.setTitle("Current: " + fps);
+			fps = 0;
+			lastFPS += 1000;
+		}
+		fps++;
+	}
+	
+	public long getTime(){
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+	
+	public int getDelta(){
+		long time = getTime();
+		int delta = (int)(time - lastFrame);
+		lastFrame = time;
+		return delta;
+	}
+	
+	public static void main(String[] args) {
+		Main myMain = new Main();
+		myMain.start();
+	}
+
+}
---
+++ src/stupid/packages/World.java
@@ -0,0 +1,56 @@
+package stupid.packages;
+
+import java.io.IOException;
+
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+public class World {
+	
+	Texture background = null;
+	
+	public World(){
+		loadData();
+	}
+	
+	public void loadData(){
+		
+		try{
+			
+			background = TextureLoader.getTexture("png", ResourceLoader.getResourceAsStream("res/bg/background_1.png"), GL11.GL_NEAREST);
+			
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}	
+	}
+	
+	public void update(int delta){
+		
+		
+		
+		drawTexture(background, 0, 0);
+	}
+	
+	public void drawTexture(Texture newTexture, int newX, int newY){
+		
+		newTexture.bind();
+		
+		GL11.glBegin(GL11.GL_QUADS);
+			
+		GL11.glTexCoord2f(0,0);
+   		GL11.glVertex2f(0,0);
+   		GL11.glTexCoord2f(1,0);
+		GL11.glVertex2f(0 + newTexture.getTextureWidth(),0);
+		GL11.glTexCoord2f(1,1);
+		GL11.glVertex2f(0+newTexture.getTextureWidth(),0+newTexture.getTextureHeight());
+		GL11.glTexCoord2f(0,1);
+		GL11.glVertex2f(0,0+newTexture.getTextureHeight());	
+    		
+    	GL11.glEnd();
+		
+	}
+	
+}
